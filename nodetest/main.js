498887e01dbea7574f80c89eec160309a3eb96d7
Irina <irina.17.95@mail.ru>
2017-04-02 13:44:24 +0700

    ajax

--- nodetest/main.js
+++ nodetest/main.js
@@ -1,54 +1,41 @@
 // Find a <table> element with id="myTable":
 var table = document.getElementById('myTable');
 
+var xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "http://localhost:3001/users";
 
-// Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(1);
+xhr.open(method, url, true);
 
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
-var cell5 = row.insertCell(4);
-
-// Add some text to the new cells:
-cell1.innerHTML = "NEW CELL1";
-cell2.innerHTML = "NEW CELL2";
-cell3.innerHTML = "NEW CELL3";
-cell4.innerHTML = "NEW CELL4";
-cell5.innerHTML = '<button class="btn btn-success" onclick="editRow()">Edit</button>';
-
-function insertRow(rowIndex) {
-
-    if (rowIndex == undefined) {
-        rowIndex = table.rows.length - 2;
-    }
-    var row = table.insertRow(rowIndex);
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-
-    cell1.innerHTML = document.getElementById('cell1').value;
-    cell2.innerHTML = document.getElementById('cell2').value;
-    cell3.innerHTML = document.getElementById('cell3').value;
-    cell4.innerHTML = document.getElementById('cell4').value;
-    cell5.innerHTML = '<button class="btn btn-success">Edit</button>';
-
-    table.deleteRow(rowIndex+1);
-
-}
-
-$(".btn-success").on("click", function(event, elem){
-    $(this).closest("tr").find("td").each(function(idx, itm){
-        console.log(itm);
-        if (idx != 4) {
-            itm.innerHTML = '<input type="text" class="form-control" id="cell' + (idx + 1) + '" size="10" value="' + itm.innerHTML + '">';
-        } else {
-            itm.innerHTML = '<button class="btn btn-info" onclick="insertRow(this.parentNode.parentNode.rowIndex)">Submit</button>';
+xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        try {
+            var data = JSON.parse(xhr.responseText);
+        } catch(err) {
+            console.log(err.message + " in " + xhr.responseText);
+            return;
         }
-    });
-});
+
+        for(var i=0; i< data.length;i++){
+
+            var row = table.insertRow(table.rows.length - 1);
+
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+
+            cell1.innerHTML = (i+1);
+            cell2.innerHTML = data[i]["name"];
+            cell3.innerHTML = data[i]["soname"];
+            cell4.innerHTML = data[i]["email"];
+            cell5.innerHTML = data[i]["birthday"];
+
+        }
+    }
+};
+
+xhr.send();
+
+
