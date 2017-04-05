83437f643d9b372ebbaab1fa03e1fd83e4f8188a
Irina <irina.17.95@mail.ru>
2017-04-05 11:08:32 +0700

    05.04.2017

--- nodetest/main.js
+++ nodetest/main.js
@@ -1,29 +1,12 @@
 // Find a <table> element with id="myTable":
 var table = document.getElementById('myTable');
 
-
-// Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(1);
-
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
-var cell5 = row.insertCell(4);
-
-// Add some text to the new cells:
-cell1.innerHTML = "NEW CELL1";
-cell2.innerHTML = "NEW CELL2";
-cell3.innerHTML = "NEW CELL3";
-cell4.innerHTML = "NEW CELL4";
-cell5.innerHTML = '<button class="btn btn-success">Edit</button>';
-
 function insertRow(value, rowIndex) {
 
     if (rowIndex == undefined) {
         rowIndex = table.rows.length - 1;
     }
+
     var row = table.insertRow(rowIndex);
 
     var cell1 = row.insertCell(0);
@@ -36,24 +19,29 @@ function insertRow(value, rowIndex) {
     cell2.innerHTML = document.getElementById('cell2').value;
     cell3.innerHTML = document.getElementById('cell3').value;
     cell4.innerHTML = document.getElementById('cell4').value;
-    cell5.innerHTML = '<button class="btn btn-success">Edit</button>';
+    cell5.innerHTML = '<button class="btn btn-success" onclick="add();">Edit</button>';
 
     if (value == true) {
 
         table.deleteRow(rowIndex + 1);
 
     }
-}
 
 
+    $(".btn-success").on("click", function (event, elem) {
+        $(this).closest("tr").find("td").each(function (idx, itm) {
 
-$(".btn-success").on("click", function (event, elem){
-    $(this).closest("tr").find("td").each(function(idx, itm){
-        if (idx != 4) {
-            itm.innerHTML = '<input type="text" class="form-control" id="cell' + (idx + 1) + '" size="10" value="' + itm.innerHTML + '">';
-        } else {
-            itm.innerHTML = '<button class="btn btn-info" onclick="insertRow(true, this.parentNode.parentNode.rowIndex)">Submit</button>';
+            if (idx != 4) {
 
-        }
+                itm.innerHTML = '<input type="text" class="form-control" id="cell' + (idx + 1) + '" size="10" value="' + itm.innerHTML + '">';
+
+            } else {
+
+                itm.innerHTML = '<button class="btn btn-info" onclick="insertRow(true, this.parentNode.parentNode.rowIndex)">Submit</button>';
+
+            }
+        });
     });
-});
+
+
+}
